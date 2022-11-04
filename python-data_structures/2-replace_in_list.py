b446ccf8047ad1dbb3a797ead8fc38a8a1cda54e
Ryan Apreala <r.apreala@alustudent.com>
2022-11-04 14:57:51 +0000

    replace at index

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
