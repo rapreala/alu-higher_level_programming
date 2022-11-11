50362a9a8c445b522d763de565d27c0cc8df784e
Ryan Apreala <r.apreala@alustudent.com>
2022-11-11 15:52:02 +0000

    class with private attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" the class square"""
+
+
+class Square:
+    """ defining the class square. """
+    def __init__(self, size):
+        """ defining the size of the square"""
+        self.__size = size
