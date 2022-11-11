8fc5d1248c9f6fc4cf8a4a6dc1d8ae789c5dc5c1
Ryan Apreala <r.apreala@alustudent.com>
2022-11-11 11:38:49 +0000

    practicing exceptions with printing list items

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for each in range(x):
+        try:
+            print(f"{my_list[each]}", end="")
+            count += 1
+        except:
+            break
+    print()
+    return (count)
