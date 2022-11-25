50759b7da19baaffc1be10af75fb50959274bb11
Ryan Apreala <r.apreala@alustudent.com>
2022-11-25 17:04:31 +0000

    5th task fix

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-'''fetch https://alu-intranet.hbtn.io/status.'''
+"""Fetch a url"""
+
 import requests
 
-if __name__ == "__main__":
 
-    r = requests.get("https://alu-intranet.hbtn.io/status")
+if __name__ == "__main__":
+    response = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
