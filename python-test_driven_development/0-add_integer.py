8bf85280dab6daf45a651e67c0e8831ae7d82683
Ryan Apreala <r.apreala@alustudent.com>
2022-12-16 14:21:04 +0000

    first

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,14 +5,11 @@
 def add_integer(a, b=98):
     """
     This function adds two integers.
-    
     Parameters:
     a (int or float): The first integer to add. Must be an integer or float.
     b (int or float): The second integer to add. Must be an integer or float. Default value is 98.
-    
     Returns:
     int: The sum of the two integers.
-    
     Raises:
     TypeError: If a or b is not an integer.
     """
@@ -21,12 +18,10 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
     # Cast a and b to integers if they are floats
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
-    
     # Return the sum of the two integers
     return a + b
