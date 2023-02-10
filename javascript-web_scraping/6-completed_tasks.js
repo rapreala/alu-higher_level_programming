1cb1f699ff66424ed65e9f2fc1378ef95dbce2be
Ryan Apreala <r.apreala@alustudent.com>
2023-02-10 11:37:43 +0000

    fix

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,25 +2,21 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.error('error:', error);
-    return;
-  }
-
-  const tasks = JSON.parse(body);
-  const userTasks = {};
-
-  for (const task of tasks) {
-    if (task.completed) {
-      if (!userTasks[task.userId]) {
-        userTasks[task.userId] = 0;
+const completed = {};
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const file = JSON.parse(body);
+    for (const data of file) {
+      if (data.completed === true) {
+        if (completed[data.userId]) {
+          completed[data.userId]++;
+        } else {
+          completed[data.userId] = 1;
+        }
       }
-      userTasks[task.userId]++;
     }
-  }
-
-  for (const userId in userTasks) {
-    console.log(`User ${userId} has completed ${userTasks[userId]} task(s)`);
+    console.log(completed);
   }
 });
