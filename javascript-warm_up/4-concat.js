85c88123b79db2ce16351893cf125357a95cdc4b
Ryan Apreala <r.apreala@alustudent.com>
2023-02-10 00:07:19 +0000

    still going

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 
-if (args.length < 2) {
-  console.log('Not enough arguments');
-} else {
-  console.log(`${args[0]} is ${args[1]}`);
-}
+console.log(`${args[0]} is ${args[1]}`);
