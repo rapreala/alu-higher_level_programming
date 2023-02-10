92a459baaeccda74d6ed5026aec8315aede4d86a
Ryan Apreala <r.apreala@alustudent.com>
2023-02-10 09:39:29 +0000

    fixes

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Square = require('./5-square.js');
+const Square1 = require('./5-square.js');
 
-class Square extends Square {
+class Square extends Square1 {
   charPrint(c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
