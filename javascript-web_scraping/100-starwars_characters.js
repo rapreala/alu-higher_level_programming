39cdd02ec2839fb09f5bf165f84e7b2852b1de7f
Ryan Apreala <r.apreala@alustudent.com>
2023-02-10 11:45:54 +0000

    advanced

---
+++ javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  const characters = data.characters;
+  characters.forEach(characterUrl => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    });
+  });
+});
---
+++ javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,50 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Invalid status code: ${response.statusCode}`);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  const characterUrls = movie.characters;
+
+  let characterIndex = 0;
+  function getNextCharacter() {
+    if (characterIndex >= characterUrls.length) {
+      return;
+    }
+
+    const characterUrl = characterUrls[characterIndex];
+    characterIndex++;
+
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        getNextCharacter();
+        return;
+      }
+
+      if (response.statusCode !== 200) {
+        console.error(`Invalid status code: ${response.statusCode}`);
+        getNextCharacter();
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+      getNextCharacter();
+    });
+  }
+
+  getNextCharacter();
+});
